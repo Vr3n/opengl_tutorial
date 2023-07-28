b8d77732a1772309e262f7ab4c657b8c46607acf
Vr3n <vickyspatel@gmail.com>
2023-07-28 19:57:04 +0530

    GLm translation examples

--- opengl_course/src/main.cpp
+++ opengl_course/src/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <glad/glad.h>
-#include <GLFW/glfw3.h>
+#include <GLFW/glfw3.h> 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
@@ -8,6 +11,22 @@ void processInput(GLFWwindow* window);
 
 int main()
 {
+
+	// glm test.
+	glm::vec4 vec(1.0f, 1.0f, 1.0f, 1.0f);
+	glm::mat4 trans = glm::mat4(1.0f); // identity Matrix.
+
+	//trans = glm::translate(trans, glm::vec3(1.0f, 1.0f, 0.0f));
+	vec = trans * vec;
+
+	// rotation.
+	trans = glm::rotate(trans, glm::radians(90.0f), glm::vec3(0.0, 0.0, 1.0));
+
+	trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5));
+
+	vec = trans * vec;
+
+	std::cout << vec.x << " " << vec.y << " " << vec.z << std::endl;
 
 	glfwInit();
 	// Define the open gl version. ours is 4.0
