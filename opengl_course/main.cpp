a07364c54e7603d5f3dea7c915defe1ca903226b
Vr3n <vickyspatel@gmail.com>
2023-07-28 12:32:08 +0530

    Add project files.

---
+++ opengl_course/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <GLFW/glfw3.h>
+#include <glad/glad.h>
+
+
+int main()
+{
+
+	glfwInit();
+	// Define the open gl version. ours is 4.0
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif // __APPLE__
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Learn Open GL", NULL, NULL);
+
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW Window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+
+	return 0;
+}
