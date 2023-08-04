f66831105eb9062efd4a13fedd082480bb0362a5
Vr3n <vickyspatel@gmail.com>
2023-08-04 18:18:15 +0530

    PointLight effect.

--- opengl_course/assets/object.fs
+++ opengl_course/assets/object.fs
@@ -28,23 +28,34 @@ uniform PointLight pointLight;
 
 uniform vec3 viewPos;
 
-void main() {
-	// FragColor = mix(texture(texture0, TexCoord), texture(texture1, TexCoord), mixVal);
 
-	// ambient
-	vec3 ambient = pointLight.ambient * vec3(texture(material.diffuse, texCoord));
+vec3 calcPointLight(vec3 norm, vec3 viewDir, vec3 diffMap, vec3 specMap);
+
+void main() {
+
+	vec3 norm = normalize(Normal);
+	vec3 viewDir = normalize(viewPos - FragPos);
+	vec3 diffMap = vec3(texture(material.diffuse, texCoord));
+	vec3 specMap = vec3(texture(material.specular, texCoord));
+
+	FragColor = vec4(calcPointLight(norm, viewDir, diffMap, specMap), 1.0);
+}
+
+vec3 calcPointLight(vec3 norm, vec3 viewDir, vec3 diffMap, vec3 specMap)
+{
+
+	vec3 ambient = pointLight.ambient * diffMap;
 
 	// diffuse
-	vec3 norm = normalize(Normal);
 	vec3 lightDir = normalize(pointLight.position - FragPos);
 	float diff = max(dot(norm, lightDir), 0.0);
-	vec3 diffuse = pointLight.diffuse * (diff * vec3(texture(material.diffuse, texCoord)));
+	vec3 diffuse = pointLight.diffuse * (diff * diffMap);
 
 	// specular
-	vec3 viewDir = normalize(viewPos - FragPos);
 	vec3 reflectDir = reflect(-lightDir, norm);
 	float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess * 128);
-	vec3 specular = pointLight.specular * (spec * vec3(texture(material.specular, texCoord)));
+	vec3 specular = pointLight.specular * (spec * specMap);
 
-	FragColor = vec4(vec3(ambient + diffuse + specular), 1.0);
+    return vec3(ambient + diffuse + specular);
+
 }
